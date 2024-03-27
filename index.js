968c71ef72996efb3a1e586f179a9ccd1dcd5a88
accarbonellweecover <albertocarlos.carbonell@weecover.com>
2024-03-27 10:29:06 -0400

    Initial Init

---
+++ index.js
@@ -0,0 +1,5 @@
+import eslint from './eslint/.eslintrc.cjs'
+import prettier from './prettier/.prettierrc.cjs'
+
+export const eslint = eslint
+export const prettier = prettier
---
+++ packages/config/prettier/.prettierrc.cjs
@@ -0,0 +1,12 @@
+module.export = {
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "none",
+  semi: true,
+  useTabs: false,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+};
