ff0863fb1b6d85c7ad442de71301c9816ee9cb0f
kidrea <amauryperez2615@gmail.com>
2024-03-30 18:15:08 -0400

    Update Tailwind and Vite configurations

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,9 +3,9 @@ const { nextui } = require('@nextui-org/react')
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    './node_modules/@nextui-org/react/dist/*.{js,ts,jsx,tsx}',
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {}
