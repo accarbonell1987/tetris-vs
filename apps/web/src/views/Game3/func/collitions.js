4dac14faef31bfe8ee4d154fe1d3eead317ec644
accarbonellweecover <albertocarlos.carbonell@weecover.com>
2024-04-03 19:37:33 -0400

    clean:  Collision

--- apps/web/src/views/Game3/func/collitions.js
+++
@@ -1,7 +0,0 @@
-export const checkCollisions = (piece, board) => {
-  return piece.shape.find((row, y) => {
-    return row.find((value, x) => {
-      return value !== 0 && board.matrix[y + piece.position.y]?.[x + piece.position.x] !== 0
-    })
-  })
-}
