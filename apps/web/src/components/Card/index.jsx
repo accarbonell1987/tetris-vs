e12baea5f940092a9259323dd9483b684770d510
accarbonellweecover <albertocarlos.carbonell@weecover.com>
2024-04-11 17:29:08 -0400

    card not hovereable

--- apps/web/src/components/Card/index.jsx
+++ apps/web/src/components/Card/index.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-
-import { Card, CardHeader, CardBody, CardFooter, Divider } from '@nextui-org/react'
+import { Card, CardHeader, CardBody, CardFooter, Divider } from '@nextui-org/react';
 
 const CustomCard = ({ header, body, footer, headDivider, footerDivider }) => {
   return (
-    <Card variant="bordered" isHoverable>
+    <Card variant="bordered" style={{ border: '1px solid #f0f0f0' }}>
       <CardHeader className="flex gap-3">{header}</CardHeader>
       {headDivider ? <Divider /> : null}
       <CardBody>{body}</CardBody>
       {footerDivider ? <Divider /> : null}
       <CardFooter>{footer}</CardFooter>
     </Card>
-  )
-}
+  );
+};
 
-export default CustomCard
+export default CustomCard;
